030accb0ba2dc28eaa2c94ad8b1b9228d37d5a3d
Surendar0601 <surendar.gaddam@slu.edu>
2025-05-07 02:54:49 +0000

    Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
+@@ -1,23 +1,26 @@
 import React from 'react'
 import { Route, Routes} from 'react-router-dom';
-
 import Header from './components/Header';
 import CardList from './components/CardList';
 import SingleView from './components/SingleView';
 import productData from './data/full-products';
+import {CartProvider} from './state/CartProvider';
+import Cart from './components/Cart';
 
 
 function App() {
-  
+
   return (
     <div className="App">
+      <CartProvider>
       <Header />
-      
+
         <Routes>
           <Route path="/" element={<CardList data={productData} />} />
           <Route path="/product/:id" element={<SingleView data={productData} />} />
+          <Route path="/" element={<CardList />} />
+          <Route path="/product/:id" element={<SingleView />} />
+          <Route path="/cart" element={<Cart />}/>
         </Routes>
-      
+
+      </CartProvider>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Config.js
@@ -0,0 +1,4 @@
+@@ -1,2 +1,2 @@
+// TODO: Replace this with your own Server URL for your codespace
+export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://organic-space-disco-v7v9r7gj5g6c66pj-3080.app.github.dev";
